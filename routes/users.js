edb0f556c18b06fe7592720cc5d6be2957c9fca6
Matija H <matija.hadelan@gmail.com>
2023-12-07 11:26:14 +0100

    handlebars

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const joi = require("joi");
 
-router.get('/Signin', function(req, res, next) {
+const { db } = require("../services/db.js");
+
+router.get('/signin', function (req, res, next) {
   res.render('users/signin');
+});
+
+const signinSchema = joi.object({
+  email: joi.string().email().max(50).required(),
+  password: joi.string().min(3).max(50).required()
+})
+
+router.post("/signin", function (req, res, next) {
+  const result = signinSchema.validate(req.body);
+  if (result.error) {
+    console.log(result.error);
+    res.sendStatus(400);
+  }
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const stmt = db.prepare("SELECT * FROM users WHERE email =? AND password= ?");
+  const dbResult = stmt.get(email, password);
+  console.log("DB kaže", dbResult);
+
+  res.render("users/signin");
+
+
 });
 
 module.exports = router;
